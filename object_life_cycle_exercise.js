d6d3e801f817767c7f0f413886ad552e37439e89
ReenaSingh07 <45616864+ReenaSingh07@users.noreply.github.com>
2019-02-07 11:43:21 +0530

    added second exercise

--- object_life_cycle_exercise.js
+++ object_life_cycle_exercise.js
@@ -22,6 +22,43 @@
     console.log(new Vec(3, 4).length);
     // → 5
 
+2]class Group {
+    constructor(a){
+    this.a=a;
+    }
+    has(m){
+    for(let i in this.a){
+    if(this.a[i] == m){
+      return true;}
+    }
+        return false;}
+    
+    add(m){
+    if(!this.has(m)){
+      this.a.push(m);}
+    }
+    delete(m){
+      let arr = [];
+       if(!this.has(m))
+       for(let i of a)
+       if(i!=m)
+          arr.push(m);
+          this.a=this.arr;
+      return this.a;
+    }  
+  }
+  
+  let group =new Group([10, 20]);
+  console.log(group.has(10));
+  // → true
+  console.log(group.has(30));
+  // → false
+  group.add(10);
+  group.delete(10);
+  console.log(group.has(10));
+  // → false
+
+
 4]  let map = {one: true, two: true, hasOwnProperty: true};
    
     
